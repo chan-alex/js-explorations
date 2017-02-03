f987bfea90c4533e4f5489d9c0274bbd9f3c9c21
alex chan <chan.k.leong@gmail.com>
2017-02-03 08:12:44 +0800

    initial commit

---
+++ es6/variables_constants_datatypes.js
@@ -0,0 +1,20 @@
+
+
+// The let keyword is new in ES6. It creates variables.
+// Before es6, the keyword to use was var.
+let x;
+let today_date = "3 Feb 2017";
+
+
+
+// const are declared with the const keyword. this is also specific to es6.
+const PI = 3.14;
+
+
+// In javascript, values are either primitives or objects.
+// These are the primitive types:
+// Numbers, String, Boolean, Null, Undefined, Symbol
+
+// Objects can be user defined or built-ins. These are the builtin objects:
+// Array, Date,  RegExp, Map (and WeakMap), Set (and WeakSet)
+
