a915cd250c5c245c311d782562bdea9979595893
alex chan <chan.k.leong@gmail.com>
2017-02-11 11:21:57 +0800

    Added control_flow.js

---
+++ es6/control_flow.js
@@ -0,0 +1,14 @@
+console.log("Start");
+
+//While loop
+
+let x1 = 0;
+while(x1 < 5) {
+    x1 = x1 + 1;
+    console.log("x1 is now " + x1);
+}
+
+let x2 = 0;
+while(x2 < 10)
+    x2 = x2 + 1;
+console.log("x2 is now " + x2);
