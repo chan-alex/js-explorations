fd95dd9748b045012089da4d0ed17ac6bb416da2
alex chan <chan.k.leong@gmail.com>
2017-02-11 20:50:44 +0800

    Added some stuff to control_flow.js

--- es6/control_flow.js
+++ es6/control_flow.js
@@ -12,3 +12,17 @@ let x2 = 0;
 while(x2 < 10)
     x2 = x2 + 1;
 console.log("x2 is now " + x2);
+
+
+// if ... else ...
+if (x2 < 20) {
+    console.log("x2 is less than 20");
+} else {
+    console.log("x2 is greater or equal to 20");
+}
+
+let x3 = 0;
+do {
+    x3 += 1;
+    console.log("x3 = " + x3);
+}while (x3 < 10);
