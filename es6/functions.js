c8609852ab69c72c493dceadc9f5b28c0df882cd
alex chan <chan.k.leong@gmail.com>
2017-02-14 08:11:45 +0800

    Added stuff to functions.js

--- es6/functions.js
+++ es6/functions.js
@@ -1,4 +1,5 @@
 
+
 // javascript function look like this:
 
 function sayhello() {
@@ -66,7 +67,7 @@ function f1(a) {
     console.log(`At end of function, a = ${a}`);
 }
 
-let a = 5
+let a = 5;
 console.log(`Before calling function, a = ${a}`);
 f1(a);
 console.log(`After calling function, a = ${a}`); // a stays at the original value.
@@ -86,3 +87,62 @@ let obj1 = { a: 5 };
 console.log(`Before calling function, obj1.a = ${obj1.a}`);
 f2(obj1);
 console.log(`After calling function, obj1.a = ${obj1.a}`); // obj1 is modified.
+
+
+
+// Unlike other language, javascript does not enforce function signatures.
+// It is possible to call a one argument function with 10 arguments.
+// Either call the same function gets called.
+
+function func1(a) {
+    console.log("From func1 with 1 argument");
+    console.log(a);
+}
+
+// Note: despite the same function being defined twice, node does not complain.
+// only eslint catches the highly likely dubious code.
+function func1(a,b) {
+    console.log("From func1 with 2 argument");    
+    console.log(a);
+    console.log(b);
+}
+
+
+func1(1);       // this actually calls the 2 argumentfunction.   
+func1(1,2);      
+func1(1,2,3,4); // the 2 argument function gets called too.
+
+
+// Another thing to notes is that it is possible to call a function without providing
+// input for the argument. This leads them to have a value of undefined.
+
+func1();  // somehow this is allowed...
+
+
+// Function arguments can be destructured.
+
+function saysomething1({word1, word2, word3}) {
+
+    console.log(`${word1} ${word2} ${word3}.`);
+
+}
+
+const words = {
+    word1: "Javascript",
+    word2: "is",
+    word3: "buggy"
+}
+
+saysomething1(words);
+
+// it is possible to do destructure array
+
+function saysomething2([word1, word2, word3]) {
+
+    console.log(`${word1} ${word2} ${word3}.`);
+
+}
+
+const words_arr = ["Javascript", "is", "buggy"];
+
+saysomething2(words_arr);
