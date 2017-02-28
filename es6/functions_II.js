4b26b293e6edba7fd52c1fb6e31c9f8618eaeea4
alex chan <chan.k.leong@gmail.com>
2017-02-28 18:52:13 +0800

    added stuff to functions_II.js

--- es6/functions_II.js
+++ es6/functions_II.js
@@ -1,32 +1,98 @@
 
 
+
+
 // setTimeout executes something after a certain timeout
 // setTimeout(function() { console.log("hello"); }, 1500);   <--- 1.5 secs.
 
-
-
-// The code below is supposed to output 5,4,3,2,1 go! but does not.
+// The code in test1() is supposed to output 5,4,3,2,1 go! but does not.
 // Question why?
-//var i;
-for (let i=5; i>=0; i--) {
-    setTimeout(function() {
-        console.log(i===0 ? "go!" : i);
-    }, (5-i)*1000);
 
-    console.log(`i = ${i}`);
+function test1() {
+    console.log("Entering test1()");
+    
+    var i;  // notice i is declared using a var.
+    for (i=5; i>=0; i--) {
+        setTimeout(function() {
+            console.log(i===0 ? "go!" : i);
+        }, (5-i)*1000);
+        
+        console.log(`i = ${i}`);  // this help to why.
+    }
 }
 
+test1(); 
+
+// Explaination
+// The loop in test1() executes 5 times. 
+// Each time it triggers setTimeout().
+// The loop will complete before even the 1st setTimeout() starts.
+// When setTimeout() starts, it will use value of "i" as that point in time.
+// which is -1 as the for loop had completed. This is due to dynamic(?) scoping.
 
 
+// The above problem is now solved with "let" and block-level scoping.
+// The code below will work correctly. Even tho the for loop will still complete
+// 1st before the 1st setTimeout() executes, the y variable this time is bound to
+// the value at the time of the firing of setTimeout()
 
-function loopbody() {
-    setTimeout(function() {
-        console.log(i===0 ? "go!" : i);
-    }, (5-i)*1000);    
+function test2() {
+    console.log("Entering test2()");
+    
+    for (let j=5; j>=0; j--) {
+        setTimeout(function() {
+            console.log(j===0 ? "go!" : j);
+        }, (5-j)*1000);
+        
+        console.log(`j = ${j}`);
+    }
 }
 
+// run test2() after a bit of timeout.
+setTimeout(test2,6000);
 
 
+// However before es6, the other way to resolve the above proble was to make use of
+// functions to capture the value of the value via function scoping.
 
 
+function test3() {
+    console.log("Entering test3()");
+    var i;  // notice i is declared using a var.
+    
+    function loop_body(i) {
+        setTimeout(function() {
+            console.log(i===0 ? "go!" : i);
+        }, (5-i)*1000);
+    }    
+    
+    for (i=5; i>=0; i--) {
+        loop_body(i);    
+        console.log(`i = ${i}`);  // this help to why.
+    }
+}
 
+// run test2() after a bit of timeout.
+setTimeout(test3,12000);
+
+
+// Creating functions just for this purpose is a bit tedious
+// esp because usually they are only going to be used once only.
+// So IIFEs were the preferred method for this sort of thing.
+
+function test4() {
+    console.log("Entering test4()");
+    var i;  // notice i is declared using a var.
+     
+    for (i=5; i>=0; i--) {
+        (function(i) {
+            setTimeout(function() {
+                console.log(i===0 ? "go!" : i);
+            }, (5-i)*1000);
+        })(i);    
+        console.log(`i = ${i}`);  // this help to why.
+    }
+}
+
+// run test2() after a bit of timeout.
+setTimeout(test4,18000);
