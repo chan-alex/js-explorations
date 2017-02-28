28adc77bfd7036889868596d5166cb0031d1dc2a
alex chan <chan.k.leong@gmail.com>
2017-02-28 19:23:09 +0800

    added some stuff and reorg in functions_II.js

--- es6/functions_II.js
+++ es6/functions_II.js
@@ -31,33 +31,11 @@ test1();
 // which is -1 as the for loop had completed. This is due to dynamic(?) scoping.
 
 
-// The above problem is now solved with "let" and block-level scoping.
-// The code below will work correctly. Even tho the for loop will still complete
-// 1st before the 1st setTimeout() executes, the y variable this time is bound to
-// the value at the time of the firing of setTimeout()
-
-function test2() {
-    console.log("Entering test2()");
-    
-    for (let j=5; j>=0; j--) {
-        setTimeout(function() {
-            console.log(j===0 ? "go!" : j);
-        }, (5-j)*1000);
-        
-        console.log(`j = ${j}`);
-    }
-}
-
-// run test2() after a bit of timeout.
-setTimeout(test2,6000);
-
-
 // However before es6, the other way to resolve the above proble was to make use of
 // functions to capture the value of the value via function scoping.
 
-
-function test3() {
-    console.log("Entering test3()");
+function test2() {
+    console.log("Entering test2()");
     var i;  // notice i is declared using a var.
     
     function loop_body(i) {
@@ -73,15 +51,15 @@ function test3() {
 }
 
 // run test2() after a bit of timeout.
-setTimeout(test3,12000);
+setTimeout(test2,6000);
 
 
 // Creating functions just for this purpose is a bit tedious
-// esp because usually they are only going to be used once only.
+// esp since usually they are only going to be used once only.
 // So IIFEs were the preferred method for this sort of thing.
 
-function test4() {
-    console.log("Entering test4()");
+function test3() {
+    console.log("Entering test3()");
     var i;  // notice i is declared using a var.
      
     for (i=5; i>=0; i--) {
@@ -91,6 +69,31 @@ function test4() {
             }, (5-i)*1000);
         })(i);    
         console.log(`i = ${i}`);  // this help to why.
+    }
+}
+
+setTimeout(test3,12000);
+
+// With the introduction of ES6,
+// The above problem is now solved with "let" and block-level scoping.
+// The code below will work correctly. Even tho the for loop will still complete
+// 1st before the 1st setTimeout() executes, the i variable this time is bound to
+// the value at the time of the firing of setTimeout()
+
+// BUT NOTE: the "let" is declared *inside* the for loop otherwise the same problem
+// will happen again. By declaring the "let i" inside the for loop, we are specifying
+// that there will be a new indepedent variable i for each step of the loop.
+
+function test4() {
+    console.log("Entering test4()");
+
+    // Notice i is declared inside the for loop. Has to here or it won't work.
+    for (let i=5; i>=0; i--) {
+        setTimeout(function() {
+            console.log(i===0 ? "go!" : i);
+        }, (5-i)*1000);
+        
+        console.log(`i = ${i}`);
     }
 }
 
