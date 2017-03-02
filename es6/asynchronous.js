992b86bb4b5b8062759046a32a0689b4532e4e64
alex chan <chan.k.leong@gmail.com>
2017-03-02 08:08:40 +0800

    add stuff to asynchronous.js

--- es6/asynchronous.js
+++ es6/asynchronous.js
@@ -37,3 +37,52 @@ setTimeout(function() {
 // Problems with callbacks
 // Callbacks hell.
 // No gurantees that error callbacks are called only once (if that was intended).
+// Erros in callbacks can bring the whole node process down (node process is just an single
+// threaded event loop.
+
+
+//Promises
+
+
+console.log("Testing promise_test1");
+
+function promise_test1(seconds) {
+
+    return new Promise( function(resolve, reject) {
+
+        if (seconds > 20) {
+            reject(new Error("Secounds incorrect value: " + seconds));
+        }
+        setTimeout(resolve, (seconds * 1000));
+
+        // the "reject" function is also called if any exceptions occurs.
+        // If the below 2 line is uncommented, reject function will be called
+        // and print out "Assignment to constant variable."
+        //  const x = 1;
+        //  x = 10;
+    });
+}
+
+promise_test1(10).then(
+    function() {
+        console.log("promise_test1 done.");
+    },
+    function(err) {
+        console.log("promise_test1 got error: " + err.message);
+    }
+);;
+
+
+
+const fs = require('fs');
+
+function readSketchyFile() {
+    try {
+        fs.readFile('does_not_exist.txt', function(err, data) {
+            if(err) throw err; });
+    } catch(err) {
+        console.log('warning: minor issue occurred, program continuing');
+    }
+}
+
+readSketchyFile();
