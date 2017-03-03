f20857554c1e52fc907960482c4cd333b20747fd
alex chan <chan.k.leong@gmail.com>
2017-03-03 22:23:33 +0800

    add

--- es6/asynchronous_2.js
+++ es6/asynchronous_2.js
@@ -164,3 +164,71 @@ function events_test1() {
 }
          
 setTimeout(events_test1, 15*1000);
+
+// events_test1() still does not stop when the unlucky number is hit.
+// Even calling the reject function will also not do it.
+// To make the promise stop, need to do a bit more.
+
+
+class Countup_v2 extends EventEmitter {
+
+    constructor(count) {
+        super();
+        this.count = count;
+        this.timeoutid = [];
+    }
+
+    
+    go() {
+
+        const countup = this;
+
+        return new Promise(function(resolve, reject) {
+            for(let i=0; i < countup.count+1; i++) {
+                countup.timeoutid.push( setTimeout(function() {
+
+                   if(i === 4) {
+                       countup.timeoutid.forEach(clearTimeout); // Clear out timer to end.
+                       reject(new Error("Unlucky number 4!"));
+                    }        
+                
+                    if(i < countup.count){
+                        console.log(`count = ${i}`);
+
+                        // This emits a "tick" event. Anyone who want to react to it
+                        // can listen to it.
+                        countup.emit("tick",i);
+                        
+                    } else {
+                        resolve(console.log("Count reached!"));
+                    }
+                }, i * 1000));
+            }
+        });
+    }
+}
+
+
+
+function events_test2() {
+
+    console.log("Running events_test2()");
+
+    const c = new Countup_v2(8);
+
+    // This attachs a listener to the "tick" event.
+    c.on('tick', function(input) {
+        console.log(`--> Still counting. Current count = ${input}`);
+    });
+
+    
+    c.go()
+        .then(function() {
+            console.log("Done!");
+        })
+        .catch(function(err) {
+            console.log(err.message);
+        });
+}
+         
+setTimeout(events_test2, 25*1000);
